41de77f0b2ca885f6a1ffb60cc5a69f867bce017
Vishal Purandare <vishalpurandare20@gmail.com>
2016-01-27 23:39:38 -0500

    check for pair in array with sum as given number

--- geeksforgeeks/src/main/java/org/vip/arrays/SumOfNumInArrayMain.java
+++ geeksforgeeks/src/main/java/org/vip/arrays/SumOfNumInArrayMain.java
@@ -1,15 +1,18 @@
 package org.vip.arrays;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SumOfNumInArrayMain {
-
-	boolean sumOfNumInArrayWithHash (int[] arr, int num) {
-		return false;
-	}
-	
+	/**
+	 * Given an array A[] and a number x, check for pair in A[] with sum as x
+	 * nlog(n) solution with sorting
+	 * @param arr
+	 * @param num
+	 * @return
+	 */
 	boolean sumOfNumInArrayWithSort (int[] arr, int num) {
-		boolean retVal = false;
 		Arrays.sort(arr);
 		int start = 0;
 		int end = arr.length - 1;
@@ -17,14 +20,63 @@ public class SumOfNumInArrayMain {
 		while (start < end) {
 			int temp = arr[start] + arr[end];
 			if (temp == num) {
-				retVal = true;
-				break;
+				return true;
 			} else if (temp < num) {
 				start++;
 			} else {
 				end--;
 			}
 		}
-		return retVal;
+		return false;
 	}
+	
+	/**
+	 * Given an array A[] and a number x, check for pair in A[] with sum as x
+	 * Naive n*n solution
+	 * @param arr
+	 * @param num
+	 * @return
+	 */
+	boolean sumOfNumInArrayWithNaive (int[] arr, int num) {
+		int len = arr.length;
+		
+		for (int idx = 0; idx < len; idx++) {
+			if (arr[idx] > num) {
+				continue;
+			}
+			for (int idx1 = idx + 1; idx1 < len; idx1++) {
+				int tempNum = arr[idx] + arr[idx1];
+				if (tempNum == num) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Given an array A[] and a number x, check for pair in A[] with sum as x
+	 * Hashmap solution, O(n) extra space, Arguably O(n) solution
+	 * @param arr
+	 * @param num
+	 * @return
+	 */
+	boolean sumOfNumInArrayWithHash (int[] arr, int num) {
+		Map<Integer, Integer> auxMap = new HashMap<Integer, Integer>();
+		
+		for (int idx = 0; idx < arr.length; idx++) {
+			if (arr[idx] > num) {
+				continue;
+			}
+			int temp = num - arr[idx];
+			if (auxMap.containsKey(arr[idx])) {
+				return true;
+			} else {
+				auxMap.put(temp, arr[idx]);
+			}
+		}
+		
+		return false;
+	}
+	
 }
--- geeksforgeeks/src/test/java/org/vip/arrays/SumOfNumInArrayMainTest.java
+++ geeksforgeeks/src/test/java/org/vip/arrays/SumOfNumInArrayMainTest.java
@@ -1,5 +1,6 @@
 package org.vip.arrays;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,11 +8,31 @@ import org.junit.Test;
 public class SumOfNumInArrayMainTest {
 
 	@Test
-	public void testSumOfNumInArray() {
+	public void testSumOfNumInArraySort() {
+		SumOfNumInArrayMain obj = new SumOfNumInArrayMain();
+		int[] arr = {1, 4, 45, 6, 10, -8};
+		int num = 11;
+		assertTrue(obj.sumOfNumInArrayWithSort(arr, num));
+	}
+	
+	@Test
+	public void testSumOfNumInArrayNaive() {
 		SumOfNumInArrayMain obj = new SumOfNumInArrayMain();
 		int[] arr = {1, 4, 45, 6, 10, -8};
 		int num = 5;
-		assertTrue(obj.sumOfNumInArrayWithSort(arr, num));
+		assertTrue(obj.sumOfNumInArrayWithNaive(arr, num));
+		num = 99;
+		assertFalse(obj.sumOfNumInArrayWithNaive(arr, num));
+	}
+	
+	@Test
+	public void testSumOfNumInArrayHash() {
+		SumOfNumInArrayMain obj = new SumOfNumInArrayMain();
+		int[] arr = {1, 4, 45, 6, 10, -8};
+		int num = 5;
+		assertTrue(obj.sumOfNumInArrayWithHash(arr, num));
+		num = 99;
+		assertFalse(obj.sumOfNumInArrayWithHash(arr, num));
 	}
 
 }
